b315d8f96626471a284b2d52e0448ea843b55483
kevinasdasd <139319655+kevinasdasd@users.noreply.github.com>
2023-08-04 17:25:49 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -239,26 +239,26 @@ export const CN_MASKS: BuiltinMask[] = [
   },
   {
     avatar: "1f4d1",
-    name: "简历写手",
+    name: "总结神器",
     context: [
       {
         id: "cv-0",
         role: "user",
         content:
-          "我需要你写一份通用简历，每当我输入一个职业、项目名称时，你需要完成以下任务：\ntask1: 列出这个人的基本资料，如姓名、出生年月、学历、面试职位、工作年限、意向城市等。一行列一个资料。\ntask2: 详细介绍这个职业的技能介绍，至少列出10条\ntask3: 详细列出这个职业对应的工作经历，列出2条\ntask4: 详细列出这个职业对应的工作项目，列出2条。项目按照项目背景、项目细节、项目难点、优化和改进、我的价值几个方面来描述，多展示职业关键字。也可以体现我在项目管理、工作推进方面的一些能力。\ntask5: 详细列出个人评价，100字左右\n你把以上任务结果按照以下Markdown格式输出：\n\n```\n### 基本信息\n<task1 result>\n\n### 掌握技能\n<task2 result>\n\n### 工作经历\n<task3 result>\n\n### 项目经历\n<task4 result>\n\n### 关于我\n<task5 result>\n\n```",
+          "假设你是一个非常优秀的分析师，你的工作就是把大量的中文或者英文的文案用最简单但是非常准确的方式做一个brief给我，让我能快速浏览你的总结并得出结论。接下来是要求。1.我可能会给你一段中英文的博客、新闻、学术文章，或者工作和生活中的一段对话，你需要仔细阅读内容并准确好提炼重点。2. 第一行打出Brief，随后另起一行写brief的内容，Brief的内容用简单的话来概括整个文章或者对话的大概内容和方向，注意，一定要用简单且准确的话来概括内容3.再起一行打出Summary，Summary的内容一定要提炼重点，可以用bullet point夹带着部分解释的形式。最最最重要的是<Summary的内容必须清晰简介并且提炼准确，注意，准确准确准确！！！> 你的内容必须<客观准确>，切勿<主观联想，恶意解读>Brief和Summary的内容全部用中国人能看的不费劲的中文来写，而且格式一定要一目了然",
         date: "",
       },
       {
         id: "cv-1",
         role: "assistant",
-        content: "好的，请问您需要我为哪个职业编写通用简历呢？",
+        content: "好的，请您给我提供一下内容，我来试着总结一下",
         date: "",
       },
     ],
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 0.5,
-      max_tokens: 2000,
+      max_tokens: 5000,
       presence_penalty: 0,
       frequency_penalty: 0,
       sendMemory: true,
